6ccbc07fa0b4918646c2d272b802748e8fa1c484
nicbongo <nicbongo@protonmail.com>
2019-12-09 18:15:54 -0400

    STRETCH

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   let [homeScore, setHomeScore] = useState(0);
+  
+  // let [awayName, setAwayName] = useState("Tigers");
   let [awayScore, setAwayScore] = useState(0);
+  
+  // game scores
+  let [quarter, setQuarter] = useState(0);
+
+
+
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -24,7 +34,9 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+
+        <BottomRow gameQuarter = {quarter} />
+
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -36,6 +48,13 @@ function App() {
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal"onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
+        <div className="gameButtons">
+
+          <button className="quarterButton" onClick={() => setQuarter(quarter + 1)}>Quarter</button>   
+
+          <button className="reset" onClick={() => [setHomeScore(0), setAwayScore(0), setQuarter(0),] }>RESET</button>       
+        </div>
+       
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState} from "react";
 import "./App.css";
 
-const BottomRow = () => {
+const BottomRow = (props) => {
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,10 +19,12 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{props.gameQuarter}</div>
       </div>
+
     </div>
   );
 };
+
 
 export default BottomRow;
